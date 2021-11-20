6b13df72069c20099f5a2792f565ec44a6db78a4
Shuntaro Nishizawa <me@shun.technology>
2021-11-20 15:34:59 +0900

    feat: Add examples instaed of test

--- examples/app/Basic.hs
+++ examples/app/Basic.hs
@@ -1,12 +1,13 @@
-module Main
-  ( main
-  ) where
+module Main where
 
 import           Graphics.WebView
 
 main :: IO ()
 main = do
-  runWebView False $ do
+  runWebView True $ do
     title "example"
     size 480 320 ResizeNone
     navigate "https://haskell.org"
+    bind "print" $ \s -> do
+      putStrLn s
+      pure . Right $ "\"print done.\""
