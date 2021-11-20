5005c5950af196588f1b4847f8973f58b3e58f01
Shuntaro Nishizawa <me@shun.technology>
2021-11-20 15:33:02 +0900

    feat: Add bind

--- src/Graphics/WebView.hs
+++ src/Graphics/WebView.hs
@@ -5,6 +5,7 @@ module Graphics.WebView
   , title
   , size
   , navigate
+  , bind
   , ResizeHint(..)
   ) where
 
@@ -22,6 +23,9 @@ foreign import ccall "webview_run" c_webview_run :: Ptr () -> IO ()
 foreign import ccall "webview_set_title" c_webview_set_title :: Ptr () -> CString -> IO ()
 foreign import ccall "webview_set_size" c_webview_set_size :: Ptr () -> CInt -> CInt -> CInt -> IO ()
 foreign import ccall "webview_navigate" c_webview_navigate :: Ptr () -> CString -> IO ()
+foreign import ccall "webview_bind" c_webview_bind :: Ptr () -> CString -> FunPtr (CString -> CString -> Ptr () -> IO ()) -> Ptr () -> IO ()
+foreign import ccall "webview_return" c_webview_return :: Ptr () -> CString -> CInt -> CString -> IO ()
+foreign import ccall "wrapper" mkBindCallback :: (CString -> CString -> Ptr () -> IO ()) -> IO (FunPtr (CString -> CString -> Ptr () -> IO ()))
 
 
 newtype WebView = WebView (Ptr ())
@@ -54,3 +58,16 @@ navigate :: String -> WebViewM ()
 navigate url = do
   (WebView w) <- ask
   liftIO . withCString url . c_webview_navigate $ w
+
+bind :: String -> (String -> IO (Either String String)) -> WebViewM ()
+bind name callback = do
+  (WebView w) <- ask
+  callback'   <- liftIO . mkBindCallback $ \seq req _ -> do
+    req' <- peekCString req
+    res  <- callback req'
+    case res of
+      Left err -> do
+        withCString err $ c_webview_return w seq 1
+      Right res' -> do
+        withCString res' $ c_webview_return w seq 0
+  liftIO . withCString name $ \name' -> c_webview_bind w name' callback' nullPtr
